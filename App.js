5383fa846fbe91c111a8c04cc805a79a958c2d2b
urielbang <uri32655@gmail.com>
2024-07-17 17:27:16 +0300

    start-app

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { useEffect, useRef } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  I18nManager,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const enableRTL = () => {
+      I18nManager.forceRTL(true);
+    };
+    enableRTL();
+
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle={"light-content"} />
+      <LinearGradient colors={["#1E3A5F", "#1F485B"]} style={styles.background}>
+        <View style={styles.content}>
+          <Text style={styles.text}>ברוכים הבאים לגיבורי אריות!</Text>
+        </View>
+
+        <View style={styles.containerImage}>
+          <Image
+            source={require("./assets/android-chrome-192x192.png")}
+            style={styles.image}
+          />
+        </View>
+
+        <Animated.View style={{ opacity: fadeAnim, alignItems: "center" }}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>לחץ כאן</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
     </View>
   );
 }
@@ -13,8 +56,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange",
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  text: {
+    backgroundColor: "transparent",
+    color: "black",
+    textAlign: "right",
+    fontFamily: "sans-serif",
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 180,
+    height: 180,
+    resizeMode: "contain",
+  },
+  containerImage: {
+    width: "100%",
+    height: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#a3731b",
+    borderRadius: 25,
+    alignItems: "center",
+    width: "50%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
